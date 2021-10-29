088f0dec4565aee2676f8bbf4c9f375af7557b05
Sandsunset <sejoon0606@gmail.com>
2021-10-29 09:23:20 +0900

    feat(database): info, get_id

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,3 @@
 from .database import Database
 from .key import new_id, to_hash
+from .session import Session
--- utils/database.py
+++ utils/database.py
@@ -7,17 +7,31 @@ class Database:
         self.conn = sqlite3.connect("database/database.db")
         self.cursor = self.conn.cursor()
 
-    def register(self, user):
+    def register(self, user) -> int:
         id = new_id()
         email = to_hash(user.email)
         pw = to_hash(user.pw)
 
-        self.cursor.execute(f" INSERT INTO User(id,email,pw,current_category) VALUES({id},{email},{pw},{user.current_category}) ")
+        self.cursor.execute(f" INSERT INTO User(id,email,pw,current_category) VALUES({id},'{email}','{pw}','{user.current_category}') ")
         self.conn.commit()
 
         return id
 
-    def exits(self, email):
+    def info(self, id) -> tuple:
+        self.cursor.execute(f" SELECT * FROM User WHERE id = '{id}'")
+        data  = self.cursor.fetchone()
+
+        return data
+
+    def get_id(self, email) -> int:
+        email = to_hash(email)
+
+        self.cursor.execute(f" SELECT * FROM User WHERE email = '{email}'")
+        id = self.cursor.fetchone()[3]
+
+        return id
+
+    def exits(self, email) -> bool:
         email = to_hash(email)
         self.cursor.execute(f" SELECT count(*) FROM User WHERE email = '{email}' ")
 
