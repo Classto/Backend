38101e269bbf0e9cf4b61a6e1295b938ba4e5686
Sandsunset <sejoon0606@gmail.com>
2021-10-29 11:04:22 +0900

    test: id test

--- key.py
+++ key.py
@@ -8,3 +8,12 @@ def new_id():
 
 def to_hash(data):
     return hashlib.sha224(bytes(data, "utf-8")).hexdigest()
+
+if __name__ == "__main__":
+    a = new_id()
+    for _ in range(10000):
+        b = new_id() 
+        if b == a:
+            print("oops")
+            break
+        print(f"attemp: {_}, {b}")
