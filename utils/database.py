ec0b8c955b94ef509634d8b79d0ed7084c2ee05d
Sandsunset <sejoon0606@gmail.com>
2021-10-29 23:17:39 +0900

    feat: search category

--- utils/database.py
+++ utils/database.py
@@ -1,6 +1,7 @@
 import sqlite3
 from key import new_id, to_hash
 
+import json
 
 class Database:
     def __init__(self):
@@ -11,10 +12,34 @@ class Database:
         id = new_id()
         email = to_hash(user.email)
         pw = to_hash(user.pw)
+        meetings = json.dumps(
+            {
+                "sample": [
+                    { 
+                        "name": "sample meeting",
+                        "time": "12:59",
+                        "link": "meeting link",
+                        "id": 1234,
+                        "password": 1234,
+                        "repeating-days" : [] 
+                    }, 
+                    {
+                        "name": "meet",
+                        "time": "00:00", 
+                        "link": "link", 
+                        "id":1, 
+                        "password": 1234, 
+                        "repeating-days" : [] 
+                    } 
+                ] 
+            }
+        )
 
         self.cursor.execute(f" INSERT INTO User(id,email,pw,current_category) VALUES({id},'{email}','{pw}','sample') ")
+        self.cursor.execute(f''' INSERT INTO Category(id,category) VALUES({id}, '["category"]') ''')
+        self.cursor.execute(f''' INSERT INTO Meetings(id,meetings) VALUES({id}, '{meetings}') ''')
         self.conn.commit()
-        
+
         return id
 
     def info(self, id) -> tuple:
@@ -38,6 +63,12 @@ class Database:
         if self.cursor.fetchone()[0] == 0:
             return False
         return True
+
+    def category(self, id) -> dict:
+        self.cursor.execute(f" SELECT * FROM Category WHERE id = '{id}'")
+        data = self.cursor.fetchone()[1]
+
+        return json.loads(data)
 
 
 if __name__ == "__main__":
